cadd3ba690d14b28b3d82c2e73232a629ae78886
Morten Hindsholm <morten@hindsholm.dk>
2016-06-28 23:54:41 +0200

    Added gpx track

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -13,22 +13,28 @@ export class MapComponent implements OnInit {
     center: ol.Coordinate = [10.1, 56.7];
 
     ngOnInit() {
+        let landMap = new ol.layer.Tile({
+            source: new ol.source.OSM()
+        });
+        let seaMap = new ol.layer.Tile({
+            source: new ol.source.XYZ({
+                url: 'http://t1.openseamap.org/seamark/{z}/{x}/{y}.png'
+            })
+        });
+        let track = new ol.layer.Vector({
+            source: new ol.source.Vector({
+                url: 'app/tracks/20140905.gpx',
+                format: new ol.format.GPX()
+            }),
+        });
+        let view = new ol.View({
+            center: ol.proj.fromLonLat(this.center, 'EPSG:3857'),
+            zoom: 12
+        });
         this.map = new ol.Map({
             target: 'map',
-            layers: [
-                new ol.layer.Tile({
-                    source: new ol.source.OSM()
-                }),
-                new ol.layer.Tile({
-                    source: new ol.source.XYZ({
-                        url: 'http://t1.openseamap.org/seamark/{z}/{x}/{y}.png'
-                    })
-                })
-            ],
-            view: new ol.View({
-                center: ol.proj.fromLonLat(this.center, 'EPSG:3857'),
-                zoom: 12
-            })
+            layers: [landMap, seaMap, track],
+            view: view
         });
     }
 
