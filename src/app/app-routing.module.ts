5aaf3303667f8e7f71b35ed8423482e241828786
Morten Hindsholm <morten@hindsholm.dk>
2017-02-14 11:56:50 +0100

    Removed an unused comma

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/trips', pathMatch: 'full' },
+    { path: '', redirectTo: '/trips', pathMatch: 'full' }
 ];
 
 @NgModule({
