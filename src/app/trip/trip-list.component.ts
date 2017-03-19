3c0d14c6bec78b6c5cb24233e5c0929eda6a2542
Morten Hindsholm <morten@hindsholm.dk>
2017-03-19 19:03:26 +0100

    Removed unnecessary stuff

--- src/app/trip/trip-list.component.ts
+++ src/app/trip/trip-list.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Track, TrackService } from './track/track.service';
-import { MapComponent } from './map/map.component';
 
 @Component({
     templateUrl: './trip-list.component.html',
@@ -8,11 +7,7 @@ import { MapComponent } from './map/map.component';
 })
 export class TripListComponent implements OnInit {
 
-    @ViewChild(MapComponent)
-    map: MapComponent;
-
     tracks: Track[] = [];
-    trackNumber: number = -1;
     errorMessage: string;
 
     constructor(private trackService: TrackService) {
@@ -23,11 +18,6 @@ export class TripListComponent implements OnInit {
             .subscribe(
                 tracks => this.tracks = tracks,
                 error => this.errorMessage = <any>error);
-    }
-
-    loadTrack(index: number) {
-        this.trackNumber = index;
-        this.map.gpx = this.tracks[index].url;
     }
 
 }
