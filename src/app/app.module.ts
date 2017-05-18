aaf389e9ccd5972ca7dcbd98644520e57120d49d
Morten Hindsholm <morten@hindsholm.dk>
2017-05-18 18:16:25 +0200

    Upgraded to Angular 4 and Material 2 beta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MaterialModule } from '@angular/material';
+import { MdButtonModule, MdMenuModule, MdToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,8 +16,11 @@ import { TripModule } from './trip/trip.module';
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         FormsModule,
-        MaterialModule.forRoot(),
+        MdButtonModule,
+        MdMenuModule,
+        MdToolbarModule,
         TripModule,
         AppRoutingModule
     ],
